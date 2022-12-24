476b447055a98b441b62e22ab9753ddb31af5466
Klucz <pawelk1245@gmail.com>
2022-12-24 22:56:54 +0100

    Simplified code

--- turtle_polygons.py
+++ turtle_polygons.py
@@ -1,31 +1,51 @@
 import turtle
+from dataclasses import dataclass
 
 
-def draw_polygon():
-    wn = turtle.Screen()
-    alex = turtle.Turtle()
-    wn.screensize(1920, 1080)
-    wn.title(wn.screensize())
+@dataclass
+class UserInput:
+    sides: int
+    size: int
+    coord_x: int
+    coord_y: int
+
+
+wn = turtle.Screen()
+plotter = turtle.Turtle()
+wn.screensize(1920, 1080)
+wn.title(wn.screensize())
+
+
+def get_user_input() -> UserInput:
     sides = int(turtle.numinput("Polygon sides", "Enter number of polygon sides", 3, minval=3, maxval=100))
-    size = turtle.numinput("Polygon size", "Enter size of a side", 30, minval=10, maxval=100)
-    coord_x = turtle.numinput("Coord X", "Enter value of coord x", 30, minval=0, maxval=1920)
-    coord_y = turtle.numinput("Coord Y", "Enter size of a coord y", 30, minval=0, maxval=1080)
-    alex.pu()
-    alex.goto(coord_x, coord_y)
-    alex.pd()
-    # def f(x,y):
-    # alex.goto(x,y)
-    # while True:
-    # alex.onclick(f)
+    size = int(turtle.numinput("Polygon size", "Enter size of a side", 30, minval=10, maxval=100))
+    coord_x = int(turtle.numinput("Coord X", "Enter value of coord x", 30, minval=0, maxval=1920))
+    coord_y = int(turtle.numinput("Coord Y", "Enter size of a coord y", 30, minval=0, maxval=1080))
+    user_input = UserInput(sides=sides, size=size, coord_x=coord_x, coord_y=coord_y)
+    return user_input
 
-    for s in range(sides):
-        alex.forward(size)
-        alex.right(180 - (((sides - 2) * 180) / sides))
+
+def initialize_plotter(user_input: UserInput):
+    plotter.pu()
+    plotter.goto(user_input.coord_x, user_input.coord_y)
+    plotter.pd()
+
+
+def draw_polygon(user_input: UserInput):
+    for s in range(user_input.sides):
+        plotter.forward(user_input.size)
+        plotter.right(180 - (((user_input.sides - 2) * 180) / user_input.sides))
+
+
+def main():
+    user_input = get_user_input()
+    initialize_plotter(user_input=user_input)
+    draw_polygon(user_input=user_input)
 
     while True:
         response = turtle.textinput("Do you want to draw another polygon?", " Type 'yes'  or 'y' or 'no' or 'n' ")
         if response == "yes" or response == "y":
-            draw_polygon()
+            main()
         elif response == "no" or response == "n":
             break
         else:
@@ -34,4 +54,4 @@ def draw_polygon():
     wn.mainloop()
 
 
-draw_polygon()
+main()
